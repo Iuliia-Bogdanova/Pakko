c0e731467e5b0e80ec7c42925d62580ccf2773bc
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-10-31 02:26:01 +0400

    slider done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-$(function(){
-
-    $('.carousel__inner').slick({
+$(".carousel__inner")
+    .slick({
         arrows: false,
         dots: true,
-        slidesToShow: 3
+        slidesToShow: 3,
+        centerMode: true,
+        centerPadding: "0px",
+        focusOnSelect: true,
+        speed: 1000,
+        cssEase: "linear"
+    })
+    .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+        if (nextSlide === 0) {
+            slick.slickGoTo(slick.slideCount - 1);
+        } else if (nextSlide === slick.slideCount - 1) {
+            slick.slickGoTo(0);
+        }
+    })
+    .on("afterChange", function (event, slick, currentSlide) {
+        let elt = slick.$slides.get(currentSlide);
+        $(".carousel__item").removeClass("active");
+        $(elt).addClass("active");
     });
-
-});
