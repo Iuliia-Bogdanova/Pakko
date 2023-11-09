711f342cd108670bca27d6ec7e7a4ec97b7de7a2
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2023-11-09 22:48:44 +0400

    mobileFirst: false add in slider

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(".carousel__inner")
-    
     .slick({
+        mobileFirst: false,
         arrows: false,
         dots: true,
         slidesToShow: 3,
@@ -14,18 +14,18 @@ $(".carousel__inner")
                 breakpoint: 940,
                 settings: {
                     slidesToShow: 2,
-                    centerMode: false
+                    centerMode: false,
                 },
             },
             {
                 breakpoint: 560,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
                 },
             },
         ],
     })
-    
+
     .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
         if (nextSlide === 0) {
             slick.slickGoTo(slick.slideCount - 1);
@@ -33,7 +33,7 @@ $(".carousel__inner")
             slick.slickGoTo(0);
         }
     })
-    
+
     .on("afterChange", function (event, slick, currentSlide) {
         let elt = slick.$slides.get(currentSlide);
         $(".carousel__item").removeClass("active");
